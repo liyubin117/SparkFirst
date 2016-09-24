4ed0dc7583e0880b230f49803fd665ce1206622b
liyubin <lixin58688@163.com>
2016-09-24 10:28:26 +0800

    init on company again

---
+++ src/com/lyb/spark/WordCount.scala
@@ -0,0 +1,26 @@
+package com.lyb.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+/**
+* 统计字符出现次数
+*/
+object WordCount {
+   def main(args: Array[String]) {
+     if (args.length < 1) {
+       System.err.println("Usage: <file>")
+       System.exit(1)
+     }
+ 
+     val conf = new SparkConf().setAppName("WordCount")
+     conf.setMaster("local[4]")
+     val sc = new SparkContext(conf)
+     val line = sc.textFile(args(0)).filter(_.startsWith("a"))
+ 
+     line.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_+_).collect().foreach(println)
+ 
+     sc.stop()
+   }
+ }
+
